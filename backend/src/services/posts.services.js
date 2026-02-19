0767101bedf484e4c1c52a53bad9b50d49099838
CecilPradhan17 <cecilpradhan99@gmail.com>
2026-02-18 21:23:46 -0600

    Create Post endpoint updated to increment replies count when answer is added to a question

--- backend/src/services/posts.services.js
+++ backend/src/services/posts.services.js
@@ -16,5 +16,38 @@ export const createPostInDB = async ({
         [content, author_id, type, parent_id]
     );
 
-    return res.rows[0];
-};
+    try {
+        await pool.query("BEGIN");
+
+        const insertRes = await pool.query(
+            `
+            INSERT INTO posts (content, author_id, type, parent_id)
+            VALUES ($1, $2, $3, $4)
+            RETURNING *;
+            `,
+            [content, author_id, type, parent_id]
+        );
+
+        const newPost = insertRes.rows[0];
+
+        // If this is an answer then increment reply_count
+        if (type === "answer" && parent_id) {
+            await pool.query(
+                `
+                UPDATE posts
+                SET reply_count = reply_count + 1
+                WHERE id = $1;
+                `,
+                [parent_id]
+            );
+        }
+
+        await pool.query("COMMIT");
+
+        return newPost;
+
+    } catch (err) {
+        await pool.query("ROLLBACK");
+        throw err;
+    };
+}
